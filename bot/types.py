8f8495f56736f53723299f0d129f78b117e545c6
huishun98 <huishun98@gmail.com>
2024-04-07 19:15:09 +0800

    Add Python backwards compatibility

--- bot/types.py
+++ bot/types.py
@@ -1,3 +1,3 @@
-from typing import Callable, Coroutine, Any
+from typing import Callable, Coroutine, Any, Optional
 
-MESSAGE_HANDLER = Callable[[Any, Any],  Coroutine[Any, Any, Exception | None]]
+MESSAGE_HANDLER = Callable[[Any, Any],  Coroutine[Any, Any, Optional[Exception]]]
