5bd83ed251c83a196d0d2da650547db25d6c7a2e
Wen Junhua <wenjh1998@gmail.com>
2024-05-11 22:44:30 +0800

    fix: fix typing using union

--- database/typing.py
+++ database/typing.py
@@ -1,6 +1,6 @@
-from typing import Any, Optional, Sequence
+from typing import Any, Optional, Sequence, Union
 from telegram import KeyboardButton
 
-ObjectType = Sequence[Sequence[str | KeyboardButton]]
+ObjectType = Sequence[Sequence[Union[str, KeyboardButton]]]
 QueryType = Optional[Any]
 CollectionType = Optional[Any]
